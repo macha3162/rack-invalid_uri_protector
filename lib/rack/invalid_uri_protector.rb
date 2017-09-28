f095c8011b7dd64869dee7b1bfbb60f5a8c67ea4
Shigeki Masuda <macha3162@gmail.com>
2017-09-28 11:32:28 +0900

    init project.

---
+++ lib/rack/invalid_uri_protector.rb
@@ -0,0 +1,28 @@
+require 'rack/invalid_uri_protector/version'
+
+module Rack
+  class InvalidUriProtector
+    SANITIZE_ENV_KEYS = %w(
+    HTTP_REFERER
+    PATH_INFO
+    REQUEST_URI
+    REQUEST_PATH
+    QUERY_STRING
+  )
+
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      SANITIZE_ENV_KEYS.each do |key|
+        string = env[key].to_s
+        valid = URI.decode(string).force_encoding('UTF-8').valid_encoding?
+        # Don't accept requests with invalid byte sequence
+        return [400, {}, ['Bad request']] unless valid
+      end
+
+      @app.call(env)
+    end
+  end
+end
